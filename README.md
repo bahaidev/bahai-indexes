ca7989b5a02ab96432e9a3e395ba5f7f3d92cd74
Brett Zamir <brettz9@yahoo.com>
2019-04-13 19:32:25 +0800

    - Linting, ignoring, package.json - Some parsing work - Initial commit (not yet functional)

---
+++ README.md
@@ -0,0 +1,14 @@
+# bahai-indexes
+
+Indexes to the Bahá'í Writings in JSON format, along with some
+tools to convert from the original (idiosyncratic) HTML versions.
+
+***NOTE: This project is not yet functional!***
+
+## Sources
+
+- `aqdas` - From <http://bahai-library.com/writings/bahaullah/aqdas/aqdas2/aqdas-allindex.html>
+
+## To-dos
+
+1. Add category mechanism
---
+++ parsers/aqdas.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const {join} = require('path');
+const {JSDOM} = require('jsdom');
+
+const html = fs.readFileSync(
+  join(__dirname, '/../indexes/html/aqdas.html'),
+  'utf8'
+);
+const {document, Node} = (new JSDOM(html)).window;
+// const $ = (s) => document.querySelector(s);
+const $$ = (s) => document.querySelectorAll(s);
+
+const letterSections = [...$$('a[name]')].filter((a) => (/^[A-Z]$/u).test(a.name))
+  .filter(({parentElement: implicitPar}) => {
+    const ul = implicitPar && implicitPar.nextElementSibling;
+    return ul && ul.matches('ul');
+  });
+const topUls = letterSections.map((
+  {parentElement: {nextElementSibling: ul}}
+) => ul);
+
+/**
+ *
+ * @param {HTMLUListElement} ul
+ * @param {Object} jsonIndexEntry
+ * @returns {undefined}
+ */
+function recurseList (ul, jsonIndexEntry) {
+  const {children: lisOrUls} = ul;
+  // console.log(lisOrUls);
+  let lastText;
+  [...lisOrUls].forEach((liOrUl, i) => {
+    if (liOrUl.matches('li')) {
+      let text = '';
+      [...liOrUl.childNodes].some((elem) => {
+        const {nodeType, nodeValue} = elem;
+        switch (nodeType) {
+        case Node.COMMENT_NODE: { // Ignore
+          break;
+        }
+        case Node.TEXT_NODE: {
+          text += nodeValue;
+          break;
+        }
+        case Node.ELEMENT_NODE: {
+          if (elem.matches('i,a[href]')) { // Ignore "see also"'s
+            // Todo: Add see also values
+            return true;
+          }
+          text += elem.textContent;
+          break;
+        }
+        default:
+          throw new TypeError('Unexpected type ' + nodeType);
+        }
+        return false;
+      });
+      jsonIndexEntry[text] = {};
+      lastText = text;
+    } else if (liOrUl.matches('ul')) {
+      jsonIndexEntry[lastText].children = {};
+      recurseList(liOrUl, jsonIndexEntry[lastText].children);
+    }
+  });
+}
+
+const jsonIndex = {};
+
+topUls.forEach((ul) => {
+  recurseList(ul, jsonIndex);
+});
+
+console.log(jsonIndex);
