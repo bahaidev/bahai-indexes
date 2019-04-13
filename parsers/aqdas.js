119f75fd5c20abc1b4c67c1cc40761e8cd5aae16
Brett Zamir <brettz9@yahoo.com>
2019-04-13 21:17:50 +0800

    - Begin distinguishing more types - Fix tag in HTML

--- parsers/aqdas.js
+++ parsers/aqdas.js
@@ -25,7 +25,7 @@ const topUls = letterSections.map((
  * @returns {string}
  */
 function stripPunctuationAndWhitespace (s) {
-  return s.trim().replace(/\s(\s+)/gu, ' ').replace(/[.,]$/gu, ''); // .replace(/\n/gu, '')
+  return s.trim().replace(/\s(\s+)/gu, ' ').replace(/[.,;]$/gu, ''); // .replace(/\n/gu, '')
 }
 
 /**
@@ -41,7 +41,10 @@ function recurseList (ul, jsonIndexEntry) {
   [...lisOrUls].forEach((liOrUl, i) => {
     if (liOrUl.matches('li')) {
       let text = '';
-      [...liOrUl.childNodes].some((elem) => {
+      let seeAlso = false;
+      let links = false;
+      const childNodes = [...liOrUl.childNodes];
+      childNodes.some((elem, i) => {
         const {nodeType, nodeValue} = elem;
         switch (nodeType) {
         case Node.COMMENT_NODE: { // Ignore
@@ -58,14 +61,14 @@ function recurseList (ul, jsonIndexEntry) {
               'See', 'See also', 'See above', 'See below',
               'See headings under'
             ].includes(elem.textContent.trim())) {
-              // Todo: Add see also values
+              seeAlso = i;
               return true;
             }
             text += `<i>${stripPunctuationAndWhitespace(elem.textContent)}</i>`;
             break;
           }
           if (elem.matches('a[href]')) {
-            // Todo: Handle links
+            links = i;
             return true;
           }
           text += elem.textContent;
@@ -79,6 +82,91 @@ function recurseList (ul, jsonIndexEntry) {
       text = stripPunctuationAndWhitespace(text);
       jsonIndexEntry[text] = {};
       lastText = text;
+      if (seeAlso !== false) {
+        jsonIndexEntry[text].$seeAlso = childNodes.slice(seeAlso).reduce(
+          (arr, l) => {
+            const {nodeType} = l;
+            switch (nodeType) {
+            case Node.COMMENT_NODE: // Ignore
+            case Node.TEXT_NODE:
+              break;
+            case Node.ELEMENT_NODE: {
+              const nodeName = l.nodeName.toLowerCase();
+              if (nodeName === 'a') {
+                arr.push(l.textContent);
+                break;
+              }
+              if (nodeName === 'br' ||
+                (nodeName === 'i' &&
+                  [
+                    // These two are used depending on whether other child
+                    //   content exists, but this can be detected and added
+                    //   programmatically
+                    'See', 'See also', 'see', 'See headings under',
+                    // "See above" and "See below" are used if the entry
+                    //   were in the same level, but this can be detected
+                    //   and added programmatically
+                    'See above', 'See below'
+                  ].includes(l.textContent))
+              ) {
+                break;
+              }
+              if (nodeName !== 'i') {
+                // Todo: Handle `ul`
+                arr.push(nodeName + ':::' + l.outerHTML);
+                break;
+              }
+              throw new TypeError('Unexpected node name');
+            }
+            default:
+              throw new TypeError('Unexpected `seeAlso` type ' + nodeType);
+            }
+            return arr;
+          },
+          []
+        );
+      }
+      if (links !== false) {
+        jsonIndexEntry[text].$links = childNodes.slice(links).reduce(
+          (arr, l) => {
+            const {nodeType} = l;
+            switch (nodeType) {
+            case Node.TEXT_NODE: {
+              const txt = stripPunctuationAndWhitespace(l.nodeValue);
+              if (txt) {
+                if (txt === '-') {
+                  // Todo: Handle range `-`
+                  arr.push('++LINKED-RANGE');
+                } else if ((/-\d+$/u).test(txt)) {
+                  // Todo: Handle range `-`
+                  arr.push('++UNLINKED-RANGE' + txt);
+                } else {
+                  // Todo: handle
+                  arr.push('==' + txt);
+                }
+              }
+              break;
+            }
+            case Node.ELEMENT_NODE: {
+              const nodeName = l.nodeName.toLowerCase();
+              if (nodeName === 'br') {
+                break;
+              }
+              const val = nodeName === 'a'
+                ? l.textContent
+                : l.nodeName + '::' + l.textContent;
+              // Todo: Handle table `nodeName` and newlines case
+              arr.push(val);
+              break;
+            }
+            default:
+              throw new TypeError('Unexpected `links` type ' + nodeType);
+            }
+            return arr;
+          },
+          []
+        );
+      }
     } else if (liOrUl.matches('ul')) {
       jsonIndexEntry[lastText].$children = {};
       recurseList(liOrUl, jsonIndexEntry[lastText].$children);
